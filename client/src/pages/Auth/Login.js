91a27d651a1fa617603f5755273e1666d816adba
Justin Rice <jsrice7391@gmail.com>
2018-04-14 16:49:41 -0400

    Shaping on the login page is working

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -2,17 +2,31 @@ import React, {Component} from "react";
 import Card from "material-ui/Card";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import {Col, Row, Grid} from "react-bootstrap";
+import TextField from 'material-ui/TextField';
+import FlatButton from "material-ui/FlatButton";
+import {Link} from "react-router-dom";
+
+
+
 
 
 
 class Login extends Component{
+    
     render(){
         return <MuiThemeProvider>
             <Grid>
               <Row>
                 <Col md={10} mdOffset={1}>
                   <Card>
-                    <h2>This is the login card</h2>
+                    <h3>Login</h3>
+                    <TextField hintText="Hint Text" floatingLabelText="Floating Label Text" />
+                    <br />
+                    <TextField hintText="Hint Text" floatingLabelText="Floating Label Text" />
+                    <br />
+                    <FlatButton label="Submit" />
+                    <br/>
+                    <Link to="/register" style={{fontSize: "12px"}}>Don't have an account?</Link>
                   </Card>
                 </Col>
               </Row>
