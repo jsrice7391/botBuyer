63327e5a402752cc4fa55756edaf97aaf34f3fb0
Justin Rice <jsrice7391@gmail.com>
2018-05-14 21:49:14 -0400

    The routing has been confirmed that it works. I got a 401 error using curl

--- routes/api/first.js
+++ routes/api/first.js
@@ -1,14 +1,54 @@
 const router = require("express").Router();
 const firstController = require("../../controllers/firstController")
-// Matches with "/api/items"
-router.route("/")
-  .get(firstController.findAll)
-  .post(firstController.create);
+var passport = require('passport');
+require('../../config/passport')(passport);
 
-// Matches with "/api/items/:id"
-router.route("/:id")
-  .get(firstController.findById)
-  .put(firstController.update)
-  .delete(firstController.remove);
+router.post('/', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    Book.create(req.body, function (err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      msg: 'Unauthorized.'
+    });
+  }
+});
+
+router.get('/', passport.authenticate('jwt', {
+  session: false
+}), function (req, res) {
+  var token = getToken(req.headers);
+  if (token) {
+    Book.find(function (err, books) {
+      if (err) return next(err);
+      res.json(books);
+    });
+  } else {
+    return res.status(403).send({
+      success: false,
+      msg: 'Unauthorized.'
+    });
+  }
+});
+
+getToken = function (headers) {
+  if (headers && headers.authorization) {
+    var parted = headers.authorization.split(' ');
+    if (parted.length === 2) {
+      return parted[1];
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
 
 module.exports = router;
