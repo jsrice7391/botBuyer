25b3f9934a65ece35070217b2f5df0d324fb0cf1
Justin Rice <jsrice7391@gmail.com>
2018-05-15 11:10:27 -0400

    Remasde the test file

---
+++ test/main.test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return the value of the index', function () {
+            assert.equal([1, 2, 3].indexOf(1), 0);
+        });
+    });
+});
